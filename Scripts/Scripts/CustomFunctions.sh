3d120f758f9b9999b864759c079ba3cdce4fa96b
Aeron Sor <53121679+AeronSor@users.noreply.github.com>
2022-02-18 11:22:11 -0300

    Update CustomFunctions.sh

--- Scripts/Scripts/CustomFunctions.sh
+++ Scripts/Scripts/CustomFunctions.sh
@@ -25,41 +25,6 @@ function encode-mp4/mov(){
     fi
 }
 
-# SSH 
-function quickSSH(){
-	echo "(1) Website send, (2) Website receive, (3) Retroarch send, (4) Retroarch send -bios-"
-	read number
-	
-	if [ $number -eq 1 ]
-	then
-		scp -r -P 777 /run/media/Hub/Projects/Coding/SSH/WaveNode/* aeron@194.195.211.207:/var/www/wavenode.tk/
-	fi
-	
-	if [ $number -eq 2 ]
-	then
-		scp -r -P 777 'aeron@194.195.211.207:/var/www/wavenode.tk/*' /run/media/Hub/Projects/Coding/SSH/WaveNode/
-	fi
-	
-	if [ $number -eq 3 ]
-	then
-		ls "/run/media/Hub/Emulators/1.Games/"
-		echo "Select the folder (exact match case)"
-		read folderNAME
-  		
-		scp -r -P 5013 /run/media/Hub/Emulators/1.Games/$folderNAME retro@10.0.0.192:Retroarch  
-	fi
-
-	if [ $number -eq 4 ]
-	then
-		ls "/run/media/Hub/Emulators/5.Bios/"
-		echo "Select system bios to send"
-		read biosFolder
-
-		scp -P 5013 /run/media/Hun/Emulators/5.Bios/$biosFolder/* retro@10.0.0.192:.config/retroarch/system
-	
-	fi
-}
-
 function amogus() {
     echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣤⣤⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠟⠉⠉⠉⠉⠉⠉⠉⠙⠻⢶⣄⠀⠀⠀⠀⠀
